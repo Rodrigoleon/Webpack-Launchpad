fa0c5412894dcfb76ee5811e9f4400ffb51527d1
Rodrigo Leon <rodrigoxleon@gmail.com>
2017-07-08 12:36:58 -0700

    Converting the Webpack config into a ES6/ES2015 function so that we can pass the ENV argument. Adding the ENV argument and adding the source maps based on prod vs dev environments.

--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,59 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
-module.exports = {
-    entry: './src/assets/js/app.js', // My main JS file with all my requires/imports. (Resets a default. Needed for this file.)
-    output: {
-        path: path.resolve(__dirname, 'app'), // My output file path. (Resets a default. Needed for this file.) Could also just be path: __dirname+'/app/js' if not using webpack-dev-server. ** Make this the dist/app folder so that all your compiled stuff goes here. This way you can let index.html be index.html and style.css be assets/css/style.css.
-        filename: 'assets/js/app.min.js' // What the name of my output file is. (Resets a default. Needed for this file.)
-        //publicPath: '/app/' // This is used if Webpack-dev-server fails but its failing to live reload anyway.
-    },
+module.exports = env => { // You may use an Object ( take out: "() => { return" ) or an ES6/ES2015 function so that we can pass the env parameter.
+    return {
+        entry: './src/assets/js/app.js', // My main JS file with all my requires/imports. (Resets a default. Needed for this file.)
+        output: {
+            path: path.resolve(__dirname, 'app'), // My output file path. (Resets a default. Needed for this file.) Could also just be path: __dirname+'/app/js' if not using webpack-dev-server. ** Make this the dist/app folder so that all your compiled stuff goes here. This way you can let index.html be index.html and style.css be assets/css/style.css.
+            filename: 'assets/js/app.min.js' // What the name of my output file is. (Resets a default. Needed for this file.)
+            //publicPath: '/app/' // This is used if Webpack-dev-server fails but its failing to live reload anyway.
+        },
+        devtool: env.prod ? 'source-map' : 'eval',
 
-    module: { // Adding Loaders.
-        rules: [
-            { // CSS Compiler (uses sass and css-loader)
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use : ['css-loader', 'sass-loader']
-                })
-            },
-            { // HTML Compiler (html-loader)
-                test: /\.html$/,
-                use : ['html-loader']
-            },
-            { // CSS Compiler (uses sass and css-loader)
-                test: /\.(jpg|png)$/,
-                use : [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets/img/' // Image Destination for App/Dist Dir.
+        module: { // Adding Loaders.
+            rules: [
+                { // CSS Compiler (uses sass and css-loader)
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use : ['css-loader', 'sass-loader']
+                    })
+                },
+                { // HTML Compiler (html-loader)
+                    test: /\.html$/,
+                    use : ['html-loader']
+                },
+                { // CSS Compiler (uses sass and css-loader)
+                    test: /\.(jpg|png)$/,
+                    use : [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'assets/img/' // Image Destination for App/Dist Dir.
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        }, // Modules End
+
+        plugins: [ // Adding Plugins + needs require(webpack); avobe if wepack is needed.
+
+            //new webpack.optimize.UglifyJsPlugin({
+            // Change settings here so that non dev one is debuggable
+            //}),
+
+            new ExtractTextPlugin("assets/css/styles.css"), // Name and Destination (It outputs it as ..css name for some reason?)
+
+            new HtmlWebpackPlugin({
+                template: 'src/index.html', // Where to look for the Source Index.html
+                filename: 'index.html' // Where to place the new index.html file for the App/Dist dir.
+            }),
+
+            new CleanWebpackPlugin(['dist']), // Removes Dist/App folder before compiling happens.
+
         ]
-    }, // Modules End
 
-    plugins: [ // Adding Plugins + needs require(webpack); avobe if wepack is needed.
-
-        new webpack.optimize.UglifyJsPlugin({
-        // Change settings here so that non dev one is debuggable
-        }),
-
-        new ExtractTextPlugin("assets/css/styles.css"), // Name and Destination (It outputs it as ..css name for some reason?)
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html', // Where to look for the Source Index.html
-            filename: 'index.html' // Where to place the new index.html file for the App/Dist dir.
-        }),
-
-        new CleanWebpackPlugin(['dist']), // Removes Dist/App folder before compiling happens.
-
-    ]
-
-};
+    };
+}
