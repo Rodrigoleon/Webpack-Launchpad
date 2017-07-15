54aac32607bb2475ca62fc6636330c72521bbbe3
Rodrigo Leon <rodrigoxleon@gmail.com>
2017-07-14 22:09:57 -0700

    Adding PurifyCSS with Glob. Fixing CSS and JS Chunk and Content Hashes. Adding source maps to all CSS files (currently only available on Build). Changing from File Loader to URL Loader so that we can use Base64. Adding fonts to the loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var webpack = require('webpack'); // Needed for Plugins that use Webpack
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var glob = require('glob'); // Goes with Purify
+var PurifyCSSPlugin = require('purifycss-webpack');
 
 module.exports = env => { // You may use an Object ( take out: "() => { return" ) or an ES6/ES2015 function so that we can pass the env parameter.
     
@@ -19,21 +21,25 @@ module.exports = env => { // You may use an Object ( take out: "() => { return" 
             
     // Pushing Plugins that are used regardless dev vs prod.
     plugins.push(
-        new ExtractTextPlugin("assets/css/styles.css?[contenthash:10]"), // Name and Destination (It outputs it as ..css name for some reason?)
+        new ExtractTextPlugin( env.prod ? "assets/css/styles.[contenthash:10].min.css" : "assets/css/styles.css" ), // Name and Destination. Note that [contenthash:10] is only available on ExtractTextPlugin
+        new PurifyCSSPlugin({ // Make sure this is after ExtractTextPlugin!
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html', // Where to look for the Source Index.html
             filename: 'index.html' // Where to place the new index.html file for the App/Dist dir.
         }),
-        new CleanWebpackPlugin(['dist']) // Removes Dist/App folder before compiling happens.
+        new CleanWebpackPlugin(['app']) // Removes Dist/App folder before compiling happens.
     );
 
     return {
-        devtool: env.prod ? 'source-map' : 'eval',
+        devtool: env.prod ? 'source-map' : 'eval', // We can use inline-source-map to have CSS Sourcemaps on Dev mode. 
         plugins: plugins, // Please note that there are alternative options. (Sec 8.)
         entry: './src/assets/js/app.js', // My main JS file with all my requires/imports. (Resets a default. Needed for this file.)
         output: {
             path: path.resolve(__dirname, 'app'), // My output file path. (Resets a default. Needed for this file.) Could also just be path: __dirname+'/app/js' if not using webpack-dev-server. ** Make this the dist/app folder so that all your compiled stuff goes here. This way you can let index.html be index.html and style.css be assets/css/style.css.
-            filename: 'assets/js/app.min.js?[hash:10]' // What the name of my output file is. (Resets a default. Needed for this file.)
+            filename: env.prod ? 'assets/js/app.[chunkhash].min.js' : 'assets/js/app.js' // What the name of my output file is. (Resets a default. Needed for this file.)
             //publicPath: '/app/' // This is used if Webpack-dev-server fails but its failing to live reload anyway.
         },
         module: { // Adding Loaders.
@@ -41,7 +47,11 @@ module.exports = env => { // You may use an Object ( take out: "() => { return" 
                 { // CSS Compiler (uses SASS-Loader and CSS-Loader and PostCSS-Loader)
                     test: /\.scss$/,
                     use: ExtractTextPlugin.extract({
-                        use : ['css-loader', 'postcss-loader', 'sass-loader']
+                        use: [ //['css-loader', 'postcss-loader', 'sass-loader']
+                            { loader: 'css-loader', options: { sourceMap: true } },
+                            { loader: 'postcss-loader', options: { sourceMap: true } },
+                            { loader: 'sass-loader', options: { sourceMap: true } }
+                        ]
                     })
                 },
                 { // HTML Compiler (html-loader)
@@ -49,13 +59,26 @@ module.exports = env => { // You may use an Object ( take out: "() => { return" 
                     use : ['html-loader']
                 },
                 { // CSS Compiler (uses sass and css-loader)
-                    test: /\.(jpg|png)$/,
+                    test: /\.(png|svg|jpg|gif)$/,
                     use : [
+                        {
+                            loader: 'url-loader', // Optionally, you can use File-Loader (It is already included in LP.)
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'assets/media/images/', // Image Destination for App/Dist Dir.
+                                limit: 10000 // This is only set when using URL Loader. (How big the file is/Greater files will come in as regular URLs).
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
                         {
                             loader: 'file-loader',
                             options: {
                                 name: '[name].[ext]',
-                                outputPath: 'assets/img/' // Image Destination for App/Dist Dir.
+                                outputPath: 'assets/media/fonts/' // Image Destination for App/Dist Dir.
                             }
                         }
                     ]
@@ -78,6 +101,5 @@ module.exports = env => { // You may use an Object ( take out: "() => { return" 
         devServer: {
             hot: env.prod ? false : true, // Tell the dev-server we're using HMR
         }
-
     };
 }
