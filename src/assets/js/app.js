d0d129ee37fb20ac2f5b4b531aaa57cbee4ac957
Rodrigo Leon <rodrigoxleon@gmail.com>
2017-07-16 17:24:44 -0700

    Setting a width to images so that sample looks somewhat presentable. Cleaning up PostCSS/Autoprefixer examples.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
 // CommonJS Imports/Requires
-require('./module-x.js'); //requires module-x JS entire file.
+require('./modules/module-x.js'); //requires module-x JS entire file.
 require('../css/style.scss'); //must be the source file not the rendered CSS.
 
 // Testing Defaults
@@ -17,17 +17,19 @@ testButton.attachEl();
 // Importing a sample image (with ES2015).
 import img from '../media/images/react-logo.png';
 // Adding the sample image (with ES5).
-app.innerHTML += '<img src="' + img + '">';
+app.innerHTML += '<img src="' + img + '" alt="Reactive" width="460">';
 
 // SVG Sprites
 import codeIcon from '../media/vectors/code.svg';
 // => {id string, width: string, height: string, viewBox: string, url: string}
 console.log(codeIcon);
 window.addEventListener('DOMContentLoaded', () => {
-  const image = `<img width="${codeIcon.width}" height="${codeIcon.height}" src="${codeIcon.url}">`;
-  // Please note that the example below does not work/spits out errors when developing locally: (https://stackoverflow.com/questions/29493965/unsafe-attempt-to-load-url-svg).
-  const usage = `<svg viewBox="${codeIcon.viewBox}"><use xlink:href="${codeIcon.url}"></use></svg>`;
-  app.innerHTML += `${image} ${usage}`;
+	// Renders <img> tag. This works on Local dev.
+	const image = `<img width="${codeIcon.width}" height="${codeIcon.height}" src="${codeIcon.url}">`;
+	// Renders SVG inline code (shadow dom).
+	// Please note that the example below does not work/spits out errors when developing locally: (https://stackoverflow.com/questions/29493965/unsafe-attempt-to-load-url-svg).
+	const usage = `<svg viewBox="${codeIcon.viewBox}"><use xlink:href="${codeIcon.url}"></use></svg>`;
+	app.innerHTML += `${image} ${usage}`;
 });
 // End of SVG Sprites.
 
